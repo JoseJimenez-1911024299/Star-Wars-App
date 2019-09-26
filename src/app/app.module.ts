f84b0706a72043654f3b49937916ef19938ac15e
JotaceSiado <39441292+JotaceSiado@users.noreply.github.com>
2019-09-26 11:53:52 -0500

    Generando Menu
    Se Ingresa el Menu de la App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule
