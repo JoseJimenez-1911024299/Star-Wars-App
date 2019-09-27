4194487fa5d022cc05af3b53e0b7d2628100a91e
JotaceSiado <39441292+JotaceSiado@users.noreply.github.com>
2019-09-27 17:46:21 -0500

    Actualización General

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-left-menu',
+  selector: 'app-left-panel',
   templateUrl: './left-panel.component.html',
   styleUrls: ['./left-panel.component.css']
 })
 export class LeftPanelComponent implements OnInit {
-
+    
   constructor() { }
 
   ngOnInit() {
