8f19af1d371bbd2938fded71e52599da7c2fc093
JotaceSiado <39441292+JotaceSiado@users.noreply.github.com>
2019-10-15 19:59:45 -0500

    Update films-service.service.ts

--- src/app/films-com/films-service.service.ts
+++ src/app/films-com/films-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Films } from './films';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
@@ -15,7 +15,30 @@ export class FilmsServiceService {
 
    }
 
-   getFilms(){
-    return this.http.get(this.apisUrl);
+   HttpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+   }
+
+   getFilms(): Observable<Films> {
+    return this.http.get<Films>(this.apisUrl + '/films')
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
   }
+  // Error handling
+  handleError(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+ }
 }
